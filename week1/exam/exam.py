d3992a1a6bc8c0156cd2289a8c87c4d0ff9ebd0c
aperrault27 <alexandre.perrault@adn.fr>
2025-06-05 16:09:07 +0200

    modification du fichier exam pour petite optimisation

--- week1/exam/exam.py
+++ week1/exam/exam.py
@@ -8,12 +8,6 @@ dest_path = '.' # keep as is
 #Déclaration de l'alphabet parce que flemme
 alphabet = list(string.ascii_lowercase)
 
-def testCode(leCodeDeuxLettres):
-    secret_password = leCodeDeuxLettres + 'bcmpda'
-    unzip_with_7z(zip_file_path, dest_path, secret_password)
-    return
-
-
 def brutForceCode():
     for lettre in product(alphabet, repeat=2):
         combinaison = ''.join(lettre) + 'bcmpda'
